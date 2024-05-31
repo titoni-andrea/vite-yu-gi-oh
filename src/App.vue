2fe162d9d96b8e205b21ecefa01351ccd23cfcde
titoni-andrea <andreatitoni@gmail.com>
2024-05-31 13:15:06 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppMinaCard from "./components/AppMainCard.vue";
+
+export default {
+components: {
+  AppHeader,
+  AppMain,
+  AppMainCard,
+  
+},
+
+  data() {
+    return {
+      prova: "ciao",
+    }
+  },
+
+  methods: {
+    ciao() {
+      console.log(this.prova);
+    }
+  },
+
+  mounted() {
+    this.ciao();
+  },
+}
+</script>
+
+<template>
+  
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<template>
+    <header class="cont-flex">
+        <h1>Yu-Gi-Oh Api</h1>
+        
+    </header>
+</template>
+
+<style scoped>
+.cont-flex {
+    display: flex;
+    align-items: center;
+}
+
+</style>
